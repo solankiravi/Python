9c8fb7e3114a5ff4a26a14aea2948ee0f178ab39
Ravi Kumar <ravikumarsinghsolanki@gmail.com>
2020-05-12 11:27:48 +0530

    Changed folder structure- added concept of id

---
+++ Python Demos/1. Introduction of Python/1. Introduction/2.Variable/2.Variable_Id_Intro.py
@@ -0,0 +1,12 @@
+x= 42
+w = 42
+y = 24
+z = x
+print(id(x))
+print(id(w))
+print(id(z))
+print(id(y))
+print(type(y))
+y = "Hello"
+print(id(y))
+print(type(y))
--- Python Demos/1. Introduction of Python/1. Introduction/2.Variable/3.Types_Of_Variable.py
+++ Python Demos/1. Introduction of Python/1. Introduction/2.Variable/3.Types_Of_Variable.py
@@ -3,10 +3,10 @@ def f():
     #To tell Python, that we want to use the global variable, we have to use the keyword "global"
     global s
     print(s) #print Python is great!
-    s = "That's clear."
-    print(s) #print That's clear.
+    s = "Value Changed."
+    print(s) #print Value Changed.
 
 
 s = "Python is great!" 
 f()
-print (s)
+print (s) #print Value Changed.
