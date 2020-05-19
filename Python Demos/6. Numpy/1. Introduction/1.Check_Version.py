cc4e5102543845b99e41eae8e175f8e416e1848f
Ravi Kumar <ravikumarsinghsolanki@gmail.com>
2020-05-19 21:54:30 +0530

    Numpy introduction started

---
+++ Python Demos/6. Numpy/1. Introduction/1.Check_Version.py
@@ -0,0 +1,3 @@
+import numpy
+#Note __version__ is an attribute not a method
+print(numpy.__version__)
---
+++ Python Demos/6. Numpy/1. Introduction/1.Getting_Started.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+list_data= [1,2,3,4,5,6]
+
+#Convert list into numpy array
+arr = np.array([list_data])
+
+print(arr)
+#numpy array is called ndarray
+print(type(arr))
