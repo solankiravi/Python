d93a48b8fd1e29a4708879c18c8d78ba5e87c24c
Ravi Kumar <ravikumarsinghsolanki@gmail.com>
2021-01-01 16:57:14 +0530

    Added using externam themes

---
+++ PythonDemos/Flask/3.using_existing_theme/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+#app.route defines the endpoints
+@app.route('/')
+@app.route('/index')
+def hello():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+#run app
+app.run(host='0.0.0.0', port='5000', debug=True)
